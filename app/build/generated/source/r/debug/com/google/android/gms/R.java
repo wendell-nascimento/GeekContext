5fad2f9218b80a870e1a33a8435ecb9536a98e6d
Nascimento <nascimento.com.br@gmail.com>
2018-04-14 12:16:57 -0400

    desenvolvimento do botão cadastrar e cancelar na activity de cadastro com auxílio da classe helper. Desenvolver o método singleton para que o usuário não abra várias instâncias na hora de logar.

--- app/build/generated/source/r/debug/com/google/android/gms/R.java
+++ app/build/generated/source/r/debug/com/google/android/gms/R.java
@@ -58,15 +58,15 @@ public final class R {
         public static final int center = 0x7f08002b;
         public static final int dark = 0x7f080039;
         public static final int icon_only = 0x7f08005a;
-        public static final int light = 0x7f080062;
-        public static final int none = 0x7f080071;
-        public static final int normal = 0x7f080072;
-        public static final int radio = 0x7f08007e;
-        public static final int standard = 0x7f0800a7;
-        public static final int text = 0x7f0800ad;
-        public static final int text2 = 0x7f0800ae;
-        public static final int wide = 0x7f0800c5;
-        public static final int wrap_content = 0x7f0800c8;
+        public static final int light = 0x7f080063;
+        public static final int none = 0x7f080072;
+        public static final int normal = 0x7f080073;
+        public static final int radio = 0x7f08007f;
+        public static final int standard = 0x7f0800a8;
+        public static final int text = 0x7f0800ae;
+        public static final int text2 = 0x7f0800af;
+        public static final int wide = 0x7f0800c6;
+        public static final int wrap_content = 0x7f0800c9;
     }
     public static final class integer {
         public static final int google_play_services_version = 0x7f090007;
