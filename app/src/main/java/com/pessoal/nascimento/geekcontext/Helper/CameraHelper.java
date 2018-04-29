2fe6e45e0486dc1042c079e1706eec871caee57a
Nascimento <nascimento.com.br@gmail.com>
2018-04-29 10:15:05 -0400

    Etapa 1 - Permissões de gravação no dispositivo

--- app/src/main/java/com/pessoal/nascimento/geekcontext/Helper/CameraHelper.java
+++ app/src/main/java/com/pessoal/nascimento/geekcontext/Helper/CameraHelper.java
@@ -1,14 +1,24 @@
 package com.pessoal.nascimento.geekcontext.Helper;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.widget.Toast;
 
 public class CameraHelper {
     //Verificar se possui permissão para acessar a camera
     //Verificar se possui permissão para salvar no dispositivo
+
+    Context context;
+    Activity activity;
+
+    public CameraHelper(Context context, Activity activity){
+        this.context=context;
+        this.activity=activity;
+    }
 
     public void VerificaPermissaoGravacao(Context context){
         if(ContextCompat.checkSelfPermission(context,
@@ -18,11 +28,15 @@ public class CameraHelper {
             Toast.makeText(context,"Permissão autorizada", Toast.LENGTH_SHORT).show();
         }
         else {
-            requestStoragePermission();
+            requestStoragePermission(activity);
         }
     }
 
-    private void requestStoragePermission() {
+    private void requestStoragePermission(Activity activity) {
+        if(ActivityCompat.shouldShowRequestPermissionRationale(activity,
+                Manifest.permission.READ_EXTERNAL_STORAGE)){
+
+        }
     }
 
 
